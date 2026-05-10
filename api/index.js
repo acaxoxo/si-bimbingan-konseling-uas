2e279cf8737569127768edde1b809124ca20ed55
Aca Asmarani <nadacantika330@gmail.com>
2026-05-10 13:28:44 +0800

    FAHHH

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const mod = await import("../backend/server.js");
   const app = mod.app || mod.default;
   return app(req, res);
